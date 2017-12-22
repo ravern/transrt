f9df11db7c96275dd8ef1f1b29e9d4465f45d714
Ravern Koh <ravern.koh.dev@gmail.com>
2017-12-22 23:09:54 +0800

    Translates the lines and ungroups them correctly

--- main.go
+++ main.go
@@ -3,15 +3,24 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	astisub "github.com/asticode/go-astisub"
+	"github.com/ravernkoh/translate-srt/deepl"
 )
 
 func main() {
 	subs, err := OpenFile("sample.srt")
 	check(err)
+	lines := ExtractLines(subs)
+	groups := GroupLinesSentences(lines)
+	groups, err = TranslateGroups(groups)
+	check(err)
+	lines = UngroupLines(groups)
 
-	_ = ExtractLines(subs)
+	for _, l := range lines {
+		fmt.Printf("%s\n\n", l)
+	}
 }
 
 func check(err error) {
@@ -64,20 +73,11 @@ func GroupLinesSentences(lines []string) [][]string {
 		// Take the first element and skip to next one if there isn't any left
 		cur := len(groups) - 1
 		groups[cur] = append(groups[cur], fst)
-		if len(sens) == 0 {
-			continue
-		}
-
-		lst := sens[len(sens)-1]
-		sens = sens[:len(sens)-1]
 
 		// Loop through and add sentences
 		for _, s := range sens {
 			groups = append(groups, []string{s})
 		}
-
-		// Add the last one
-		groups = append(groups, []string{lst})
 	}
 
 	return groups
@@ -114,14 +114,68 @@ func SplitAfter(s string, delims []byte) []string {
 	return ret
 }
 
-// TranslateGroup translates a single group
-func TranslateGroup(group []string) ([]string, error) {
-	return nil, nil
+// TranslateGroups translates a slice of groups
+func TranslateGroups(groups [][]string) ([][]string, error) {
+	req := deepl.NewDeepl()
+	for _, g := range groups {
+		req.AddJob(strings.Join(g, ""))
+	}
+
+	res, err := req.Request()
+	if err != nil {
+		return nil, err
+	}
+
+	// Split result by proportion
+	ret := [][]string{}
+	for i := range res.Result.Translations {
+		// Get the text of the best result
+		text := res.Result.Translations[i].Beams[0].PostprocessedSentence
+		length := totalLength(groups[i])
+		group := []string{}
+
+		for _, s := range groups[i][:len(groups[i])-1] {
+			// Take this number of chars from the text
+			take := int(float32(len(s)) / float32(length) * float32(len(text)))
+			group = append(group, text[:take])
+			text = text[take:]
+		}
+
+		// For the last item, just append the remainding chars
+		group = append(group, text)
+
+		ret = append(ret, group)
+	}
+
+	return ret, nil
+}
+
+// totalLength of a slice of strings
+func totalLength(sl []string) int {
+	var ret int
+	for _, s := range sl {
+		ret += len(s)
+	}
+	return ret
 }
 
 // UngroupLines converts the groups back into their ungrouped state
 func UngroupLines(groups [][]string) []string {
-	return nil
+	lines := []string{""}
+
+	for _, g := range groups {
+		fst := g[0]
+		g = g[1:]
+
+		cur := len(lines) - 1
+		lines[cur] += fst
+
+		for _, c := range g {
+			lines = append(lines, c)
+		}
+	}
+
+	return lines
 }
 
 // InsertLines inserts the lines back into the subtitles object
